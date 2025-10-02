c24aae53d0ef2289de2a6ec7457e51c92842c2d2
Maurício V. <mauriciovieira5878@gmail.com>
2025-10-02 13:23:35 -0300

    atividade14

---
+++ atividade14.py
@@ -0,0 +1,6 @@
+# Crie uma tupla com os dias da semana e exiba o terceiro e o sexto dia.
+
+diassemana = ('segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo')
+
+print(diassemana[0])
+print(diassemana[6])
